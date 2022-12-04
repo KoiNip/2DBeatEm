cc1982a0d71a36ee3298cd4c1e0eaa2aa9cc2e22
fdhernandez <rodriguezhernandez0406@gmail.com>
2022-12-04 15:33:26 -0800

    fixing errors with camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(5f, 2f, -10f);
+    private Vector3 offset = new Vector3(3f, 2f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
