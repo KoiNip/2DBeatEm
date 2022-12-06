6f059684835efc644abd488d812ec6cecf84141d
fdhernandez <rodriguezhernandez0406@gmail.com>
2022-12-05 18:34:18 -0800

    adding comments to camera and finish scripts

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,6 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*** 
+*file: CameraFollow.cs 
+*author: Flor Hernandez 
+*class: CS 4700 – Game Development 
+*assignment: Final Project 
+*date last modified: 12/4/2022 
+* 
+*purpose: This code makes sure the camera follows the player
+*as they move through the levels and is set to a certain offset
+*within the player's range
+* 
+**/
+
 public class CameraFollow : MonoBehaviour
 {
     private Vector3 offset = new Vector3(3f, 0f, -10f);
--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*** 
+*file: Finish.cs 
+*author: Flor Hernandez 
+*class: CS 4700 – Game Development 
+*assignment: Final Project 
+*date last modified: 12/5/2022 
+* 
+*purpose: This code calls the next scene in the build settings 
+*once the player reaches an object that is triggered. The player
+*will spawn in the next scene in the build index.
+* 
+**/
+
 public class Finish : MonoBehaviour
 {
     //private AudioSource finishSound:
